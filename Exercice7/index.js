7d7255ddcb1fe6c6418dd5bf963cf67267a6e110
mystekerinos <jordy.saintalbin@gmail.com>
2024-08-06 21:09:03 +0200

    fix-exercice-even

--- Exercice7/index.js
+++ Exercice7/index.js
@@ -1,5 +1,12 @@
 function convertToBinary() {
   let decimalInput = document.getElementById("decimalInput").value.trim();
+
+  // Vérifiez si l'entrée contient des caractères non numériques
+  if (!/^\d+$/.test(decimalInput)) {
+    document.getElementById("binaryResult").textContent = "";
+    return;
+  }
+
   let decimalNumber = parseInt(decimalInput, 10);
 
   if (isNaN(decimalNumber) || decimalNumber < 0) {
@@ -20,7 +27,5 @@ function convertToBinary() {
     binaryResult = "0";
   }
 
-  document.getElementById(
-    "binaryResult"
-  ).textContent = `Résultat binaire: ${binaryResult}`;
+  document.getElementById("binaryResult").textContent = binaryResult;
 }
